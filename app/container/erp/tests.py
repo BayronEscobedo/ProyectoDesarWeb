c5ba620d2e8f5ef4fa8183efbe2a8cc96d6b0769
Bayron <bramose@miumg.edu.gt>
2020-10-10 07:30:39 -0600

    Avanzado hasta video 25

--- app/container/erp/tests.py
+++ app/container/erp/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from app.wsgi import *
+from container.erp.models import Type
 
-# Create your tests here.
+# Listar
+#Type.objects.all()
+
+# insertar
+
+#t = Type(name='Presidente').save()
+
+#Editar
+#t =Type.objects.get(pk=1)
+#t.name='Accionista'
+#t.save()
+
+#Eliminar
+#t=Type.objects.get(pk=1)
+#t.delete()
+
+#Listar con filtros incluyendo mayusculas y minusculas
+
+#obj = Type.objects.filter(name__icontains='Ac')
+#print(obj)
